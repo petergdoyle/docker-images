333c06fbc04b70c338e07191509be449e990a3ed
vagrant <vagrant@docker-images.vbx>
2015-09-17 16:31:20 -0400

    cleaned up server startup scripts to build the properties for multiple servers on the fly rather than statically set

--- kafka/2.9/start-kafka-consumer.sh
+++ kafka/2.9/start-kafka-consumer.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: start-kafka-consumer [zk'localhost:2181'] [topic]"
+  exit 1
+fi
 #$KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic airshop --from-beginning
 $KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper $zk --topic $topic --from-beginning
--- kafka/2.9/start-kafka-singlenode.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties 
